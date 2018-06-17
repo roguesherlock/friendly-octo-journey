a5310a58f17937981f8cc6e63f41a12e36b7a992
akash <pomalakash@gmail.com>
2018-06-17 23:13:47 +0530

    Updated Rotate Matrix to use functions from Matrix module

--- Interview Questions/Arrays and Strings/rotateMatrix.js
+++ Interview Questions/Arrays and Strings/rotateMatrix.js
@@ -7,70 +7,7 @@
  *  Hints:#51, #100
  */
 
-/**
- * createMatrix
- * @param {number} n row
- * @param {number} m cloumns
- * @return {array} an NxM matrix
- * @description creates an NxM matrix
- */
-function createMatrix(n, m) {
-    let matrix = [];
-    for (let i = 0; i < n; ++i) {
-        matrix.push(new Array(m));
-    }
-    return matrix;
-}
-
-/**
- * fillMatrix
- * @param {array} matrix an NxM matrix
- * @param {number} max integer
- * @return {array} matrix filled with random
- * values
- * @description fills matrix with random
- * values between 0 and max
- */
-function fillMatrix(matrix, max) {
-    if (matrix.length < 1) {
-        return matrix;
-    }
-    matrix.forEach((row) => {
-        /* make sure it's a mtrix after all */
-        if (!row instanceof Array) {
-            return matrix;
-        }
-        for (let i = 0; i < row.length; ++i) {
-            row[i] = Math.floor(Math.random() * Math.floor(max));
-        }
-    });
-    return matrix;
-}
-
-/**
- * checkMatrices
- * @param {array} matrix1
- * @param {array} matrix2
- * @return {boolean} are matrices equal
- * @description checks if two matrices are equal
- * or not
- */
-function checkMatrices(matrix1, matrix2) {
-    if (matrix1.length != matrix2.length) {
-        return false;
-    }
-    for (let i = 0; i < matrix1.length; ++i) {
-        if (matrix1[i].length != matrix2[i].length) {
-            return false;
-        }
-        for (let j = 0; j < matrix1[i].length; ++j) {
-            if (matrix1[i][j] != matrix2[i][j]) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
+const Matrix = require('../../Data Structures/Matrix.js');
 
 /**
  * rotateMatrix
@@ -88,7 +25,7 @@ function rotateMatrix(matrix) {
     }
 
     /* we're expecting NxN matrix */
-    let newMatrix = createMatrix(mLen, mLen);
+    let newMatrix = Matrix.createMatrix(mLen, mLen);
 
     for (let i = 0; i < mLen; ++i) {
         if (mLen != matrix[i].length) {
@@ -101,8 +38,8 @@ function rotateMatrix(matrix) {
     return newMatrix;
 }
 
-let m = createMatrix(4, 4);
-let n = createMatrix(4, 4);
+let m = Matrix.createMatrix(4, 4);
+let n = Matrix.createMatrix(4, 4);
 m[0] = [1, 2, 3, 4];
 m[1] = [5, 6, 7, 8];
 m[2] = [9, 10, 11, 12];
@@ -113,4 +50,4 @@ n[1] = [14, 10, 6, 2];
 n[2] = [15, 11, 7, 3];
 n[3] = [16, 12, 8, 4];
 let o = rotateMatrix(m);
-console.log(checkMatrices(n, o));
+console.log(Matrix.checkMatrices(n, o));
